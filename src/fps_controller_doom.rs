5f22d0b663e3ab7253b6d9578acc8eda39dd1210
TimotyEnder <timotyender@gmail.com>
2026-06-08 14:18:55 +0200

    aaaa

--- src/fps_controller_doom.rs
+++ src/fps_controller_doom.rs
@@ -17,6 +17,7 @@ impl ScriptBehavior for FpsControllerDoom {
     fn update(&mut self, gameobject: &mut crate::ecs::gameobject::GameObject, delta_time: f64) {
         let rotation_speed = 180.0_f64.to_radians();
         let rotation_speed: f64 = rotation_speed * delta_time;
+        let movement_speed = 10.0 * delta_time;
         if let Some(body) = gameobject.get_component_mut::<Body>() {
             if INPUT.lock().unwrap().is_down(KeyCode::ArrowRight) {
                 body.rotate((0.0, rotation_speed, 0.0));
@@ -25,16 +26,16 @@ impl ScriptBehavior for FpsControllerDoom {
                 body.rotate((0.0, -rotation_speed, 0.0));
             }
             if INPUT.lock().unwrap().is_down(KeyCode::KeyW) {
-                body.translate_based_on_direction(1.0, 0.0, 0.0);
+                body.translate_based_on_direction(movement_speed, 0.0, 0.0);
             }
             if INPUT.lock().unwrap().is_down(KeyCode::KeyS) {
-                body.translate_based_on_direction(-1.0, 0.0, 0.0);
+                body.translate_based_on_direction(-movement_speed, 0.0, 0.0);
             }
             if INPUT.lock().unwrap().is_down(KeyCode::KeyA) {
-                body.translate_based_on_direction(0.0, 1.0, 0.0);
+                body.translate_based_on_direction(0.0, movement_speed, 0.0);
             }
             if INPUT.lock().unwrap().is_down(KeyCode::KeyD) {
-                body.translate_based_on_direction(0.0, -1.0, 0.0);
+                body.translate_based_on_direction(0.0, -movement_speed, 0.0);
             }
         }
     }
